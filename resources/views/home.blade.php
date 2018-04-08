b5e189d93e2417a48ba1a21a9f6fd49fa618af37
UnclePluto <dypluto@163.com>
2018-04-08 11:09:41 +0800

    页面本地化

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,8 +5,10 @@
     <div class="uk-container uk-container-expand">
 
         <div class="uk-card uk-card-primary uk-card-body uk-width-1-2@m uk-align-center">
-            <h3 class="uk-card-title">Dashboard</h3>
-            <p>You are logged in!</p>
+            <h3 class="uk-card-title">主页</h3>
+            <p>您已登录!</p>
+            <p>这里应该是显示工具箱首页了!</p>
+            <p>但是完全没有头绪呢。。。</p>
         </div>
 
     </div>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,9 +3,6 @@
 <style>
     html, body {
         background-color: #fff;
-        color: #636b6f;
-        font-family: 'Raleway', sans-serif;
-        font-weight: 100;
         height: 100vh;
         margin: 0;
     }
@@ -55,11 +52,11 @@
         </div>
 
         <div class="links">
-            <a href="https://laravel.com/docs">Documentation</a>
-            <a href="https://laracasts.com">Laracasts</a>
-            <a href="https://laravel-news.com">News</a>
+            <a href="http://faq.netease.im/mctools.html">原工具箱</a>
+            <a href="http://faq.netease.im">FAQ</a>
+           {{-- <a href="https://laravel-news.com">News</a>
             <a href="https://forge.laravel.com">Forge</a>
-            <a href="https://github.com/laravel/laravel">GitHub</a>
+            <a href="https://github.com/laravel/laravel">GitHub</a> --}}
         </div>
     </div>
 </div>
